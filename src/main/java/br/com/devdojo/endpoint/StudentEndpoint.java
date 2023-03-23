b2a4dfd8d635fb3874f24da8a87946afcd59b0bd
Ajax Lima <ajaxlima94@gmail.com>
2023-03-23 16:09:23 -0300

    Aula15 - Paginação em requisições REST

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -6,6 +6,7 @@ import br.com.devdojo.model.Student;
 import br.com.devdojo.repository.StudentRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,9 +25,8 @@ public class StudentEndpoint {
     }
 
     @GetMapping
-    public ResponseEntity<?> listAll() {
-        //System.out.println(this.dateUtil.formatLocalDatetimeToDatabaseStyle(LocalDateTime.now()));
-        return new ResponseEntity<>(studentDAO.findAll(), HttpStatus.OK);
+    public ResponseEntity<?> listAll(Pageable pageable) {
+        return new ResponseEntity<>(studentDAO.findAll(pageable), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
